c3e5a176f3611299cb11dbd41dee691c9948bad7
ThomasTheFknTrainEngine <marten.uiboupin@gmail.com>
2025-01-20 09:50:08 +0200

    error easter egg

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/StatusBar.css";
 import { FaCodeBranch } from 'react-icons/fa';
 
 const StatusBar = () => {
   const githubLink = "https://github.com/TheVaxly/code-portfolio";
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleErrorClick = () => {
+    setClickCount(prev => prev + 1);
+  };
+
+  const getErrorText = () => {
+    return clickCount >= 69 ? "69 Errors  420 Warnings" : "0 Errors  0 Warnings";
+  };
 
   return (
     <div className="status-bar">
@@ -12,7 +21,9 @@ const StatusBar = () => {
         <span className="icon"><FaCodeBranch /></span>
         <span className="main-text">main</span>
       </div>
-      <span className="error">0 Errors  0 Warnings</span>
+      <span className="error" onClick={handleErrorClick} style={{ cursor: "pointer" }}>
+        {getErrorText()}
+      </span>
     </div>
   );
 };
